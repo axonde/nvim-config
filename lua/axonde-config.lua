35bb0706a1f28b32a148ef8291853049f1a3cb20
axonde <alex.chpak@gmail.com>
2025-08-16 18:18:45 +0800

    [keyboard] add russian keyboad by <C-Space> + color cursor support

--- lua/axonde-config.lua
+++ lua/axonde-config.lua
@@ -10,7 +10,66 @@ vim.keymap.set("n", "<leader>j", "<C-w>j")
 vim.keymap.set("n", "<leader>k", "<C-w>k")
 vim.keymap.set("n", "<leader>l", "<C-w>l")
 
--- lazy.nvim
+-- Russian keyboard settings
+vim.opt.keymap = "russian-jcukenwin"
+vim.opt.iminsert = 0
+_G.keyboard_layout = "EN"
+
+local function change_cursor_color_lnk_keyboard()
+	if vim.fn.has("macunix") ~= 1 or vim.fn.has("gui_running") == 1 then
+		return
+	end
+	local color = _G.keyboard_layout == "RU" and "6D37FF" or "00FF00"
+	local escape_seq = string.format("\27]Pl%s\27\\", color)
+	vim.fn.chansend(vim.v.stderr, escape_seq)
+end
+
+local function toggle_keyboard_layout()
+	local new_iminsert = vim.o.iminsert == 0 and 1 or 0
+	vim.o.iminsert = new_iminsert
+	_G.keyboard_layout = new_iminsert == 1 and "RU" or "EN"
+	change_cursor_color_lnk_keyboard()
+
+	if vim.fn.has("gui_running") == 1 then
+		vim.cmd("redraw")
+	end
+
+	local current_mode = vim.fn.mode()
+
+	if current_mode == "i" then
+		vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>a", true, false, true), "n", false)
+	end
+
+	if package.loaded["lualine"] then
+		require("lualine").refresh()
+	end
+end
+
+vim.api.nvim_create_autocmd("VimLeave", {
+	callback = function()
+		if vim.o.iminsert == 1 then
+			toggle_keyboard_layout()
+		end
+	end,
+})
+
+vim.api.nvim_set_keymap("i", "<C-Space>", "", {
+	noremap = true,
+	silent = true,
+	callback = toggle_keyboard_layout,
+})
+vim.api.nvim_set_keymap("n", "<C-Space>", "", {
+	noremap = true,
+	silent = true,
+	callback = toggle_keyboard_layout,
+})
+vim.api.nvim_set_keymap("x", "<C-Space>", "", {
+	noremap = true,
+	silent = true,
+	callback = toggle_keyboard_layout,
+})
+
+-- Lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,11 +1,34 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-        require('lualine').setup({
-            options = {
-                theme = 'dracula'
-            }
-        })
-    end
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "auto",
+				-- component_separators = { left = "", right = "" },
+				-- section_separators = { left = "", right = "" },
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = { "filename" },
+				lualine_x = {
+					"encoding",
+					"fileformat",
+					-- "filetype",
+					{
+						function()
+							return _G.keyboard_layout
+						end,
+						color = function()
+							return { fg = _G.keyboard_layout == "RU" and "#ff5555" or "#50fa7b" }
+						end,
+						icon = { " ", align = "right" },
+					},
+				},
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+		})
+	end,
 }
